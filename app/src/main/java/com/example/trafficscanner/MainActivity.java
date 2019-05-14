3b10a372d112d1930fdaf2587d71614830d334e2
Thijs de Jong <thijsg.dejong@gmail.com>
2019-05-14 19:28:31 +0200

    Fixed APK

--- app/src/main/java/com/example/trafficscanner/MainActivity.java
+++ app/src/main/java/com/example/trafficscanner/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.trafficscanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        Button main_button = findViewById(R.id.main_button);
+
+        main_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+    }
+
+    public void openCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
