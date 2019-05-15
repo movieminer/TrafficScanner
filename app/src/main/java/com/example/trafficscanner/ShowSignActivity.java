eceeeef7f1551c9f06f674f85d7e7d4f889cea6b
Thijs de Jong <thijsg.dejong@gmail.com>
2019-05-15 14:23:51 +0200

    Fixed APK

---
+++ app/src/main/java/com/example/trafficscanner/ShowSignActivity.java
@@ -0,0 +1,23 @@
+package com.example.trafficscanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ShowSignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_show_sign);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
+    }
+}
